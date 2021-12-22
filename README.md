9d1ce5d75aee2b948186bfeadc63403ee61c1975
Alexandre Miguel <alexandrefreitasmiguel@alunos.utfpr.edu.brgit config --global user.name Alexandre>
2021-12-22 11:41:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+<h1>Buscador de letras 🎵</h1>
+
+### 📑 Sobre 
+<p>Esse Projeto é um app simples para buscar letras de músicas atrávez de uma API.</p>
+
+### 💻 Demonstração 
+
+---
+
+<img style="max-width: 320px; border-radius: 10px;" src="https://cdn.discordapp.com/attachments/922557394419056671/922882486529319003/demo.gif">
+
+---
+
+### 📱 Interface
+
+<img style="max-height: 350px; border-radius: 10px;" src="https://cdn.discordapp.com/attachments/922557394419056671/922893824928383006/demoMobile.png">
+<img style="max-height: 350px; border-radius: 10px;"src="https://cdn.discordapp.com/attachments/922557394419056671/922893838685724682/demoMobileLyrics.png">
+
+---
+
+### ⚙️ Funcionalidades 
+
+- Pesquisar a música através do input
+- Ver a capa do album e nome do artista/música
+- Ver a letra da música ao clicar no botão "ver mais"
+
+---
+
+### Pré-requesitos
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[GIT](https://git-scm.com/), [node.js](https://nodejs.org/en/).
+Além disto, é bom ter um editor para trabalhar com o código como [VScode](https://code.visualstudio.com/);
+
+### 🎲 Rodando a aplicação
+
+```bash
+# clone este repositório
+$ git clone 
+
+# acesse a pasta do projeto no terminal
+$ cd buscar-letras
+
+# instale as dependências
+$ npm
+
+# Execute a aplicação em modo de desemvolvimento
+$ npm start 
+
+# servidor iniciará na porta: 3000 - <http://localhost:3000>
+```
+---
+Desenvolvido 💜 por Alexandre Miguel [See my LinkedIn](https://www.linkedin.com/in/alexandre-miguel-6969b6209/)
---
+++ js/app.js
@@ -0,0 +1,106 @@
+const $ = document.querySelector.bind(document);
+
+const form = $('#form');
+const searchInput = $('#search')
+const songsContainer = $('#songs-container')
+const prevAndNextContainer = $('#prev-and-next-container')
+
+const apiURL = `https://api.lyrics.ovh`
+
+const fetchData = async url => {
+   try {
+      const res = await axios.get(url);
+      return res.data;
+   } catch (err) {
+      insertErrorMessage('Erro: Não foi possível obter o resultado da letra para esse título.');
+   }
+}
+
+const insertErrorMessage = (msg) => {
+   $('.popup-error').classList.add('popup-error--ativo');
+   $('.error-msg').textContent = msg;
+   setTimeout(() => {
+      $('.popup-error')
+         .classList.remove('popup-error--ativo');
+   }, 2000);
+}
+
+const getMoreSongs = async url =>
+   insertSongsIntoPage(await fetchData(`https://cors-anywhere.herokuapp.com/${url}`));
+
+const insertLyricsIntoPage = ({ lyrics, artist, songTitle }) => {
+   songsContainer.innerHTML = `
+      <li class="lyrics-container">
+         <h2><strong>${songTitle}</strong> - ${artist}</h2>
+         <p class="lyrics">${lyrics}</p>
+      </li>
+   `
+}
+
+const fetchLyrics = async (artist, songTitle) => {
+   const data = await fetchData(`${apiURL}/v1/${artist}/${songTitle}`);
+   if (data === undefined) return;
+   const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+   insertLyricsIntoPage({ lyrics, artist, songTitle })
+}
+
+const insertNextAndPrevButtons = ({ prev, next }) => {
+   prevAndNextContainer.innerHTML = `
+   ${prev ? `<button class="btn hvr-sweep-to-right" onClick="getMoreSongs('${prev}')">Anteriores</button>` : ''}
+   ${next ? `<button class="btn hvr-sweep-to-right" onClick="getMoreSongs('${next}')">Próximas</button>` : ''}
+`
+}
+
+const insertSongsIntoPage = ({ data, prev, next }) => {
+   songsContainer.innerHTML = data.map(({ artist: { name }, title, album }) => `
+      <li class="song">
+         <img src="${album.cover_medium}">
+         <span class="song-artist"><strong>${name}</strong> - ${title}</span>
+         <button class="btn hvr-sweep-to-right" data-artist="${name}" data-song-title="${title}">Ver letra</button>
+      </li>
+      `).join('');
+
+   prev || next
+      ? insertNextAndPrevButtons({ prev, next })
+      : prevAndNextContainer.innerHTML = '';
+}
+
+const fetchSongs = async term =>
+   insertSongsIntoPage(await fetchData(`${apiURL}/suggest/${term}`));
+
+const validateTerm = term => {
+   term
+      ? fetchSongs(term)
+      : insertErrorMessage('Erro: Insira um termo válido.');
+}
+
+const formatInput = input => input.normalize("NFD")
+   .replace(/[\u0300-\u036f]/g, "");
+
+const handleFormSubmit = event => {
+   event.preventDefault();
+   const inputValue = searchInput.value.trim();
+   const searchTerm = formatInput(inputValue);
+   searchInput.focus();
+   validateTerm(searchTerm)
+}
+
+const handleSongsContainerClick = event => {
+   const clickedElement = event.target;
+
+   if (clickedElement.tagName === 'BUTTON') {
+      const artist = clickedElement.getAttribute('data-artist');
+      const songTitle = clickedElement.getAttribute('data-song-title');
+
+      prevAndNextContainer.innerHTML = '';
+      fetchLyrics(artist, songTitle)
+   }
+}
+
+form.addEventListener('submit', handleFormSubmit);
+songsContainer.addEventListener('click', handleSongsContainerClick);
+
+let coffee;
+
+
+!coffee ? coffee.refill() : coffee.drink();
